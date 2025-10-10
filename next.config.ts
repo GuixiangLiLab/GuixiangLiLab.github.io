fa16979bf6f41e885d1bf8ee7277b326e9c7b9ae
MerciCH <113984015+MerciCH@users.noreply.github.com>
2025-10-10 19:49:28 +0800

    repo-test

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   // GitHub Pages 二级路径支持（actions/configure-pages 会注入这个 env）
-  basePath: process.env.PAGES_BASE_PATH || '',
-  assetPrefix: process.env.PAGES_BASE_PATH || '',
+  assetPrefix: "",
+  basePath: "",
   images: {
     unoptimized: true,
   },
